072963d25547df9af8ef46ea3441b500e9a0c4e3
caiogmrocha <caioguilherme739@gmail.com>
2022-07-10 14:56:54 -0300

    test: ensure CreateUserUseCase to be able to create a new user

---
+++ src/modules/create-user/create-user-use-case.spec.ts
@@ -0,0 +1,25 @@
+import { InMemoryCreateUserRepository } from "../../repositories/users/create-user/in-memory-create-user-repository"
+import { ICreateUserDTO } from "./create-user-dto"
+import { CreateUserUseCase } from "./create-user-use-case"
+
+const makeSut = () => {
+  const createUserRepository = new InMemoryCreateUserRepository()
+  const sut = new CreateUserUseCase(createUserRepository)
+
+  return { sut, createUserRepository }
+}
+
+describe('Create User Use Case', () => {
+  it('should be able to create a new user', async () => {
+    const { sut } = makeSut()
+    const data: ICreateUserDTO = {
+      name: 'any_name',
+      email: 'any_email',
+      password: 'any_password'
+    }
+
+    const result = await sut.execute(data)
+
+    expect(result.isSuccess()).toBeTruthy();
+  })
+})
