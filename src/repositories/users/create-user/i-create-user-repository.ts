ead0bc42fb1a174be77dbf500540e4f82e6c2058
caiogmrocha <caioguilherme739@gmail.com>
2022-07-10 14:56:02 -0300

    feat: add InMemoryCreateUserRepository

---
+++ src/repositories/users/create-user/i-create-user-repository.ts
@@ -0,0 +1,6 @@
+import { User } from '../../../entities/user'
+import { ICreateUserDTO } from '../../../modules/create-user/create-user-dto'
+
+export interface ICreateUserRepository {
+  execute(data: ICreateUserDTO): Promise<User>
+}
---
+++ src/repositories/users/create-user/in-memory-create-user-repository.ts
@@ -0,0 +1,15 @@
+import { User } from '../../../entities/user'
+import { ICreateUserDTO } from '../../../modules/create-user/create-user-dto'
+import { ICreateUserRepository } from './i-create-user-repository'
+
+export class InMemoryCreateUserRepository implements ICreateUserRepository {
+  private users: User[] = []
+
+  async execute (data: ICreateUserDTO): Promise<User> {
+    const user = new User(data)
+
+    this.users.push(user)
+
+    return user
+  }
+}
