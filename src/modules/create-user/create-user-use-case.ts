074debde7e5f38b2335aa8b63d0c392e8da58594
caiogmrocha <caioguilherme739@gmail.com>
2022-07-10 14:56:43 -0300

    feat: ensure CreateUserUseCase to be able to create a new user

---
+++ src/modules/create-user/create-user-use-case.ts
@@ -0,0 +1,30 @@
+import { User } from '../../entities/user'
+import { Either, failure, success } from '../../helpers/logic/Either'
+import { ICreateUserRepository } from '../../repositories/users/create-user/i-create-user-repository'
+import { ICreateUserDTO } from './create-user-dto'
+
+export class CreateUserUseCase {
+  private createUserRepository: ICreateUserRepository
+
+  constructor (createUserRepository: ICreateUserRepository) {
+    this.createUserRepository = createUserRepository
+  }
+
+  async execute (data: ICreateUserDTO): Promise<Either<Error, User>> {
+    if (!data.name) {
+      return failure(new Error('Missing param: name'))
+    }
+
+    if (!data.email) {
+      return failure(new Error('Missing param: email'))
+    }
+
+    if (!data.password) {
+      return failure(new Error('Missing param: password'))
+    }
+
+    const createdUser = await this.createUserRepository.execute(data)
+
+    return success(createdUser)
+  }
+}
