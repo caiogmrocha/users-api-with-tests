ef469fb8a9ad9e20cb13cf4405cb0e1e55ff729c
caiogmrocha <caioguilherme739@gmail.com>
2022-07-23 13:13:49 -0300

    chore: update eslint settings

--- src/modules/create-user/create-user-use-case.ts
+++ src/modules/create-user/create-user-use-case.ts
@@ -1,30 +1,30 @@
-import { User } from '@/entities/user'
-import { Either, failure, success } from '@/helpers/logic/Either'
-import { ICreateUserRepository } from '@/repositories/users/create-user/i-create-user-repository'
-import { ICreateUserDTO } from './create-user-dto'
+import { User } from '@/entities/user';
+import { Either, failure, success } from '@/helpers/logic/Either';
+import { ICreateUserRepository } from '@/repositories/users/create-user/i-create-user-repository';
+import { ICreateUserDTO } from './create-user-dto';
 
 export class CreateUserUseCase {
-  private createUserRepository: ICreateUserRepository
+  private createUserRepository: ICreateUserRepository;
 
   constructor (createUserRepository: ICreateUserRepository) {
-    this.createUserRepository = createUserRepository
+    this.createUserRepository = createUserRepository;
   }
 
   async execute (data: ICreateUserDTO): Promise<Either<Error, User>> {
     if (!data.name) {
-      return failure(new Error('Missing param: name'))
+      return failure(new Error('Missing param: name'));
     }
 
     if (!data.email) {
-      return failure(new Error('Missing param: email'))
+      return failure(new Error('Missing param: email'));
     }
 
     if (!data.password) {
-      return failure(new Error('Missing param: password'))
+      return failure(new Error('Missing param: password'));
     }
 
-    const createdUser = await this.createUserRepository.execute(data)
+    const createdUser = await this.createUserRepository.execute(data);
 
-    return success(createdUser)
+    return success(createdUser);
   }
 }
