8b6c28d39f69d178af701a64737301e3ac75820f
caiogmrocha <caioguilherme739@gmail.com>
2022-07-23 13:21:11 -0300

    feat: ensure User entity validate data correctly

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,3 +1,9 @@
+import { objectIsEmpty } from '@/helpers';
+import { Either, failure, success } from '@/helpers/logic/Either';
+import { ValidationError } from '@/validation/errors/validation-error';
+import { RequiredFieldValidator } from '@/validation/rules/required-field';
+import { ValidationCompositor } from '@/validation/validation-compositor';
+
 export interface IUserProps {
   id?: string;
   name: string;
@@ -6,13 +12,31 @@ export interface IUserProps {
 }
 
 export class User {
-  props: IUserProps
+  props: IUserProps;
 
   constructor (props: IUserProps) {
     if (!props.id) {
-      props.id = `${Date.now()}`
+      props.id = `${Date.now()}`;
     }
 
-    this.props = props
+    this.props = props;
+  }
+
+  public validate (): Either<ValidationError, any> {
+    const { name, email, password } = this.props;
+
+    const validationCompositor = new ValidationCompositor([
+      new RequiredFieldValidator('nome', name),
+      new RequiredFieldValidator('email', email),
+      new RequiredFieldValidator('senha', password)
+    ]);
+
+    const result = validationCompositor.validate();
+
+    if (objectIsEmpty(result.errors)) {
+      return success({});
+    }
+
+    return failure(result);
   }
 }
