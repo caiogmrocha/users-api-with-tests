4f974ae12f760afde1aa392a5dc36ad818dec0c8
caiogmrocha <caioguilherme739@gmail.com>
2022-08-06 17:04:27 -0300

    test: remove validate from User entity unit test

--- src/entities/user.spec.ts
+++ src/entities/user.spec.ts
@@ -10,28 +10,4 @@ describe('User Entity', () => {
 
     expect(sut.props).toHaveProperty('id')
   })
-
-  it('should be able to validate a User data' , () => {
-    const sut = new User({
-      name: 'John Doe',
-      email: 'john@doe.com',
-      password: 'any_password',
-    });
-
-    const result = sut.validate()
-
-    expect(result.isRight()).toBeTruthy()
-  })
-
-  it('should throw if a invalid User data is provided' , () => {
-    const sut = new User({
-      name: '',
-      email: '',
-      password: '',
-    });
-
-    const result = sut.validate()
-
-    expect(result.isLeft()).toBeTruthy()
-  })
 })
