73137b6acaaacb8f4704660a1eff569ab8a57be7
caiogmrocha <caioguilherme739@gmail.com>
2022-08-08 21:03:59 -0300

    refactor: typo

--- src/validation/rules/required-field.spec.ts
+++ src/validation/rules/required-field.spec.ts
@@ -2,7 +2,7 @@ import { RequiredFieldError } from "../errors/required-field-error"
 import { RequiredFieldValidator } from "./required-field"
 
 describe('Required Field Validator', () => {
-  it('should throw if a empty string is provided', () => {
+  it('should return RequiredFieldError if a empty string is provided', () => {
     const anyField = ''
     const sut = new RequiredFieldValidator('any_field', anyField)
 
@@ -11,7 +11,7 @@ describe('Required Field Validator', () => {
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
-  it('should throw if null is provided', () => {
+  it('should return RequiredFieldError if null is provided', () => {
     const anyField = null
     const sut = new RequiredFieldValidator('any_field', anyField)
 
@@ -20,7 +20,7 @@ describe('Required Field Validator', () => {
     expect(error).toEqual(new RequiredFieldError('any_field'))
   })
 
-  it('should throw if undefined is provided', () => {
+  it('should return RequiredFieldError if undefined is provided', () => {
     const anyField = undefined
     const sut = new RequiredFieldValidator('any_field', anyField)
 
