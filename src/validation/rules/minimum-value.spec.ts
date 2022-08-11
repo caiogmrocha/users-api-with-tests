9d960efaa30cae4812f91ee773bdc5ad5876f430
caiogmrocha <caioguilherme739@gmail.com>
2022-08-10 21:38:05 -0300

    test: ensure MinimumValueValidator returns MinimumValueError if a valid string is provided

--- src/validation/rules/minimum-value.spec.ts
+++ src/validation/rules/minimum-value.spec.ts
@@ -12,6 +12,16 @@ describe('Minimum Value Validator', () => {
     expect(error).toEqual(new MinimumValueError('name', anyField, minimumValue))
   })
 
+  it('should return undefined if a valid string is provided', () => {
+    const anyField = 'John Doe';
+    const minimumValue = 8
+    const sut = new MinimumValueValidator('name', anyField, minimumValue);
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined()
+  })
+
   it('should return MinimumValueError if a invalid number is provided', () => {
     const anyField = 100;
     const minimumValue = 199
@@ -20,5 +30,15 @@ describe('Minimum Value Validator', () => {
     const error = sut.validate();
 
     expect(error).toEqual(new MinimumValueError('price', anyField, minimumValue))
+  })
+
+  it('should return undefined if a valid number is provided', () => {
+    const anyField = 100;
+    const minimumValue = 99
+    const sut = new MinimumValueValidator('price', anyField, minimumValue);
+
+    const error = sut.validate();
+
+    expect(error).toBeUndefined()
   })
 })
