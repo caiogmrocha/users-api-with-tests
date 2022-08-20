10cdacd765d24995ad8e12105bbc8a7d1de9d21a
caiogmrocha <caioguilherme739@gmail.com>
2022-08-19 22:38:37 -0300

    test: ensure DeleteUserController returns 404 if user does not exists

---
+++ src/modules/delete-user/delete-user-controller.test.ts
@@ -0,0 +1,33 @@
+import { app } from '@/app';
+import { PrismaCreateUserRepository } from '@/repositories/users/create-user/prisma-create-user-repository';
+import { UserNotFoundError } from '@/repositories/users/errors/user-not-found-error';
+import request from 'supertest';
+import { CreateUserUseCase } from '../create-user/create-user-use-case';
+
+describe('[e2e] Delete User Controller', () => {
+  beforeAll(async () => {
+    const prismaCreateUserRepository = new PrismaCreateUserRepository();
+    const createUserUseCase = new CreateUserUseCase(prismaCreateUserRepository);
+
+    await createUserUseCase.execute({
+      name: 'John Doe',
+      email: 'john@doe.com',
+      password: 'password'
+    });
+
+    await createUserUseCase.execute({
+      name: 'Ana Victoria',
+      email: 'ana@bell.com',
+      password: 'password'
+    });
+  })
+
+  it('should return 404 if user does not exists', async () => {
+    const response = await request(app).delete('/users/3')
+
+    expect(response.status).toBe(404)
+    expect(response.body).toEqual({
+      error: { name: UserNotFoundError.name }
+    })
+  })
+})
